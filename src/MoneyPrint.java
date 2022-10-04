412c6f5cc19262212c4186fdd5bc61f347aaf335
Qkite <yeonjigu4@gmail.com>
2022-10-04 14:19:12 +0900

    화폐개수 출력하기

--- src/MoneyPrint.java
+++ src/MoneyPrint.java
@@ -3,13 +3,27 @@ public class MoneyPrint {
         int refund = 25000;
         int[] moneyArray = new int[]{50000,10000,5000,1000,500,100,50,10};
 
-        for (int money:
-             moneyArray) {
-            System.out.print(money + "원: ");
-            int num = refund / money;
-            System.out.print(num + "개");
-            System.out.println();
-            refund = refund % money;
+        int curr50000 = 50000;
+        int curr10000 = 10000;
+        int curr5000 = 5000;
+        int curr1000 = 1000;
+        int curr500 = 500;
+        int curr100 = 100;
+
+        System.out.println("50000원: " + refund/curr50000);
+        System.out.println("10000원: " + refund/curr10000);
+        System.out.println("5000원: " + refund/curr5000);
+        System.out.println("1000원: " + refund/curr1000);
+        System.out.println("500원: " + refund/curr500);
+        System.out.println("100원: " + refund/curr100);
+
+//        for (int money:
+//             moneyArray) {
+//            System.out.print(money + "원: ");
+//            int num = refund / money;
+//            System.out.print(num + "개");
+//            System.out.println();
+//            refund = refund % money;
 
         }
     }
