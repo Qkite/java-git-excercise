4f0182a5553f17f40f9d5825d3b5aaa6e5786000
Qkite <yeonjigu4@gmail.com>
2022-10-04 13:24:43 +0900

    array 커밋합니다~

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,6 +1,9 @@
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int[] array = new int[10];
+        for (int i=0; i<10; i++){
+            array[i] = i;
+        }
         for (int i=0; i<array.length; i++){
             System.out.println(array[i]);
         }
